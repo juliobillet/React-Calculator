079040b41008e382a065ed610127df3b17d77877
Julio Billet <juliobillet@gmail.com>
2022-09-27 05:05:36 -0300

    Minor changes 05:05 27-09-2022

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
-import './App.css';
+import './calculator.css';
 
-function App() {
+function Calculator() {
   return (
-    <div className="App">
+    <div className="calculator">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default Calculator;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import calculator from './calculator';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<calculator />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
